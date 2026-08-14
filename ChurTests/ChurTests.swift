64c757a199d53c37a3ad97a8e9578d3bcad54c13
Pak Ho <hopakho1@gmail.com>
2026-08-14 15:57:57 -0700

    created churtests

---
+++ ChurTests/ChurTests.swift
@@ -0,0 +1,18 @@
+//
+//  ChurTests.swift
+//  ChurTests
+//
+//  Created by Pak Ho on 8/14/26.
+//
+
+import Testing
+
+struct ChurTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
